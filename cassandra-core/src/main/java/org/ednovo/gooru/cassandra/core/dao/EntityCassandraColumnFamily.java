54520f96cd5ed6e897eff9bb493fcf92f9a8e145
Renuka <renuka@goorulearning.org>
2015-10-29 11:21:06 +0530

    Added consistency level for reading

--- cassandra-core/src/main/java/org/ednovo/gooru/cassandra/core/dao/EntityCassandraColumnFamily.java
+++ cassandra-core/src/main/java/org/ednovo/gooru/cassandra/core/dao/EntityCassandraColumnFamily.java
@@ -10,6 +10,7 @@ import org.ednovo.gooru.core.cassandra.model.ReverseIndexColumnSetting;
 
 import com.netflix.astyanax.Keyspace;
 import com.netflix.astyanax.entitystore.EntityManager;
+import com.netflix.astyanax.model.ConsistencyLevel;
 
 /**
  * @author SearchTeam
@@ -37,7 +38,7 @@ public class EntityCassandraColumnFamily<M> extends CassandraColumnFamily {
 		setColumnFamilyName(initColumnFamilyName(clazz));
 		super.init(keyspace);
 		if (keyspace != null) {
-			entityManager = new GooruDefaultEntityManager.Builder<M, String>().withEntityType(clazz).withKeyspace(keyspace).build();
+			entityManager = new GooruDefaultEntityManager.Builder<M, String>().withEntityType(clazz).withKeyspace(keyspace).withConsistency(ConsistencyLevel.CL_QUORUM).build();
 		} else {
 			getLog().error("Cassandra Mapper for " + getColumnFamilyName() + " : FAILED");
 		}
